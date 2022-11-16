8a09ddd40e9b5870d70bfd8043a63df143b0096a
aleksandr_eremeev <eremeev.san@yandex.ru>
2022-11-16 13:48:22 +0300

    spring-mvc

---
+++ CourseSpringBoot/MVC/appendix/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+@SpringBootConfiguration
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
+
+//
+//
+//
+//
+//
+//
